8903c25695ae9d08d675a69ade2222f84303aff3
edwin-tarazona <80715680+Edwin-Papasito@users.noreply.github.com>
2022-11-21 00:26:18 -0500

    for

--- ciclos.py
+++ ciclos.py
@@ -6,3 +6,13 @@ for contador in range(1,10):
 for contador in range(1,10):
     print(contador,end=" ")
     
+#iterar 1
+frutas = {'Fresa':'roja', 'Limon':'verde', 'Papaya':'naranja'}
+for fruta,color in frutas.items():
+    print(fruta, "es de color", color)
+    
+#iterar 2
+for fruta1 in frutas:
+    print(fruta1, "es de color", frutas[fruta1])
+    
+######while
